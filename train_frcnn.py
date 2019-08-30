ce42d6409e825ad7340d0e21aa44d7bf24a43fec
unknown <ttp.pun@gmail.com>
2019-08-30 09:38:49 +0700

    edit input_weight_path

--- train_frcnn.py
+++ train_frcnn.py
@@ -34,7 +34,7 @@ parser.add_option("--config_filename", dest="config_filename", help=
 				"Location to store all the metadata related to the training (to be used when testing).",
 				default="config.pickle")
 parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='/content/gdrive/My Drive/model_frcnn.hdf5')
-parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights. If not specified, will try to load default weights provided by keras.")
+parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights.", default='/content/gdrive/My Drive/model_frcnn.hdf5')
 
 (options, args) = parser.parse_args()
 
