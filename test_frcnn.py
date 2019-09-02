1e6a0eb699931b624e7f8dd46e4c92e4fd6ea6ac
unknown <ttp.pun@gmail.com>
2019-09-02 15:14:51 +0700

    f

--- test_frcnn.py
+++ test_frcnn.py
@@ -245,6 +245,6 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	#cv2.imshow('img', img)
 	#cv2.waitKey(0)
 	#cv2.imwrite('/content/{}.jpg'.format(idx),img)
-	name=str(format(idx))+'.jpg'
-	print(img_path)
-	cv2.imwrite('/content/result_img/'+name,img)
+	#name=str(format(idx))+'.jpg'
+	
+	cv2.imwrite('/content/result_img/'+img_name,img)
