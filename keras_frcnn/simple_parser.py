1406e1544bc2fff581918725e406a5ba3c93ae7c
unknown <ttp.pun@gmail.com>
2019-09-13 15:56:31 +0700

    read input image

--- keras_frcnn/simple_parser.py
+++ keras_frcnn/simple_parser.py
@@ -35,7 +35,7 @@ def get_data(input_path):
 				
 				img = cv2.imread(filename,-1)
 				# print(filename)
-				_, th_image = cv2.threshold(raw_image, 0, 1, cv2.THRESH_TOZERO)
+				_, th_image = cv2.threshold(img, 0, 1, cv2.THRESH_TOZERO)
 				uint8_image = th_image * 255
 				img=uint8_image
 				(rows,cols) = img.shape[:2]
