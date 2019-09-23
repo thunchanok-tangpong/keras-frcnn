bbc82d2932135d3ab29700cde8e3e778b4738a08
unknown <ttp.pun@gmail.com>
2019-09-23 10:12:13 +0700

    rac

--- test_frcnn.py
+++ test_frcnn.py
@@ -238,7 +238,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 
 			(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
 
-			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
+			cv2.rectangle(img1,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
 
 			textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
 			xmin=real_x1
