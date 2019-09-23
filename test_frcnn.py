9640dd9b2c5ac1b721a0577012fc102d7b01110e
unknown <ttp.pun@gmail.com>
2019-09-23 09:31:51 +0700

    imread in test

--- test_frcnn.py
+++ test_frcnn.py
@@ -156,8 +156,14 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	st = time.time()
 	filepath = os.path.join(img_path,img_name)
 
-	img = cv2.imread(filepath, -1)
+	img = cv2.imread(img_data_aug['filepath'], -1)
+	_, img = cv2.threshold(img, 0, 1, cv2.THRESH_TOZERO)
+	img = img * 255
+	img=img.astype(np.uint8)
 	img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+
+	# img = cv2.imread(filepath, -1)
+	# img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
 
 	X, ratio = format_img(img, C)
 
