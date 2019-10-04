bc6dcb78e412982f8750d41ed5af0305e850a203
unknown <ttp.pun@gmail.com>
2019-10-04 12:19:06 +0700

    update loading model path

--- test_frcnn.py
+++ test_frcnn.py
@@ -134,7 +134,7 @@ model_classifier_only = Model([feature_map_input, roi_input], classifier)
 
 model_classifier = Model([feature_map_input, roi_input], classifier)
 
-print('Loading weights from {}'.format(C.model_path))
+print('Loading weights from {}'.format('/content/drive/My Drive/model_frcnn.hdf5'))
 model_rpn.load_weights(C.model_path, by_name=True)
 model_classifier.load_weights(C.model_path, by_name=True)
 
