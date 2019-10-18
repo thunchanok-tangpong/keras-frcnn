6b0f6c6fde823eba50e4fecd4b7f72096c4ac3fe
unknown <ttp.pun@gmail.com>
2019-10-18 13:27:40 +0700

    ffrger

--- train_frcnn.py
+++ train_frcnn.py
@@ -120,7 +120,8 @@ if K.image_dim_ordering() == 'th':
 	input_shape_img = (3, None, None)
 	print("th")
 else:
-	input_shape_img = (None, None, 3)
+	# input_shape_img = (None, None, 3)
+	input_shape_img = (5,None, None)
 	print("not th")
 
 img_input = Input(shape=input_shape_img)
