7015904eca110b570b7b8cf43508641f8f59be0a
unknown <ttp.pun@gmail.com>
2019-10-18 15:13:45 +0700

    ffff

--- keras_frcnn/data_generators.py
+++ keras_frcnn/data_generators.py
@@ -349,7 +349,7 @@ def get_anchor_gt(all_img_data, class_count, C, img_length_calc_function, backen
 				x_img /= C.img_scaling_factor
 
 				# x_img = np.transpose(x_img, (2, 0, 1))
-				x_img = np.transpose(x_img, (4,2, 0, 1,3))
+				x_img = np.transpose(x_img, (4,0,1,2,3))
 				x_img = np.expand_dims(x_img, axis=0)
 
 				y_rpn_regr[:, y_rpn_regr.shape[1]//2:, :, :] *= C.std_scaling
