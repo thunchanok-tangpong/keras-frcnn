3b5649da1abaf153709647e832b4c1cff8977278
unknown <ttp.pun@gmail.com>
2019-10-18 11:19:20 +0700

    f

--- keras_frcnn/data_generators.py
+++ keras_frcnn/data_generators.py
@@ -279,7 +279,7 @@ def get_anchor_gt(all_img_data, class_count, C, img_length_calc_function, backen
 	n=0
 	while True:
 		n=n+1
-		print(n)
+		# print(n)
 		if mode == 'train':
 			np.random.shuffle(all_img_data)
 
