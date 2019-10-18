c0df0d00feccd769484c4ff2679d9bd62cba74e0
unknown <ttp.pun@gmail.com>
2019-10-18 11:24:17 +0700

    ff

--- keras_frcnn/data_generators.py
+++ keras_frcnn/data_generators.py
@@ -306,10 +306,12 @@ def get_anchor_gt(all_img_data, class_count, C, img_length_calc_function, backen
 
 				# get image dimensions for resizing
 				(resized_width, resized_height) = get_new_img_size(width, height, C.im_size)
+				print(resized_width)
+				print(resized_height)
 
 				# resize the image so that smalles side is length = 600px
 				x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
-				print(x_img.shape)
+				print(x_img)
 
 				try:
 					y_rpn_cls, y_rpn_regr = calc_rpn(C, img_data_aug, width, height, resized_width, resized_height, img_length_calc_function)
