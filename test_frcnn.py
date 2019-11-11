7b56cea4fd34ad5c280b7c77e1921662f918c22f
unknown <ttp.pun@gmail.com>
2019-11-11 12:27:53 +0700

    dd

--- test_frcnn.py
+++ test_frcnn.py
@@ -183,6 +183,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	img =np.transpose(myarray2, (1, 2, 0))
 	img1=img*5
 	img1=img1[:,:,(0,1,2)]
+	img1 = img1.astype(np.uint8).copy()
 
 	X, ratio = format_img(img, C)
 	# print(X.shape)
